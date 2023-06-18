26c74b06c42f824195156a4c1691887ec7e2d990
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-17 22:13:37 -0500

    state variables

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { API_KEY } from "./utils.js";
+//state
+
+let currCity='Lima,PE'
+let units='M'
+
 function getWeather() {
-  fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=Lima,PE&days=5&key=${API_KEY}`).then(res=>res.json()).then(data=>console.log(data))
+  fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${currCity}&days=5&units=${units}&key=${API_KEY}`).then(res=>res.json()).then(data=>console.log(data))
 }
 
 getWeather()

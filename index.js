91e86194e9e17c1b785322a58cfda6239a1bbd40
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-18 08:20:55 -0500

    function getDateByTimezone

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ let units='M'
 //selector
 
 let city = document.querySelector('.weather__city')
+let dateTime = document.querySelector('.weather__datetime')
 
 //change code country to name country
 function convertCountryCode(country){
   let regionNames = new Intl.DisplayNames(["en"], {type: "region"});
   return regionNames.of(country)
 }
+//date 
+function getTimeByZone(timezone){
+  const date = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: `${timezone}`, hour12:true};
+  return date.toLocaleString('en-US', options);
+}
+
 
 
 
@@ -20,9 +28,9 @@ function getWeather() {
   fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${currCity}&days=5&units=${units}&key=${API_KEY}`).then(res=>res.json()).then(data=>{
     console.log(data);
     city.innerHTML = `${data.city_name}, ${convertCountryCode(data.country_code)}`
+    dateTime.innerHTML = getTimeByZone(data.timezone)
   })
   
-
 }
 
 getWeather()

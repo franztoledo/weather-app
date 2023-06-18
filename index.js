8fde419949c44bddada7480b868b711c734a016a
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-17 22:28:20 -0500

    change code country to name country

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ let units='M'
 
 let city = document.querySelector('.weather__city')
 
+//change code country to name country
+function convertCountryCode(country){
+  let regionNames = new Intl.DisplayNames(["en"], {type: "region"});
+  return regionNames.of(country)
+}
+
 
 
 function getWeather() {
   fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${currCity}&days=5&units=${units}&key=${API_KEY}`).then(res=>res.json()).then(data=>{
     console.log(data);
-    city.innerHTML = `${data.city_name}, ${data.country_code}`
+    city.innerHTML = `${data.city_name}, ${convertCountryCode(data.country_code)}`
   })
   
 

9963a5d2efb7ead894e7eec4b9476c8d56412d5e
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-17 22:26:24 -0500

    name of city

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ let city = document.querySelector('.weather__city')
 
 
 function getWeather() {
-  fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${currCity}&days=5&units=${units}&key=${API_KEY}`).then(res=>res.json()).then(data=>console.log(data))
+  fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${currCity}&days=5&units=${units}&key=${API_KEY}`).then(res=>res.json()).then(data=>{
+    console.log(data);
+    city.innerHTML = `${data.city_name}, ${data.country_code}`
+  })
+  
 
 }
 

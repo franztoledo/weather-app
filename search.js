237323b2b3377e72bcff29f505dac88f8bd28d28
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-19 04:07:57 -0500

    keys and selectors for the search matchlist

---
+++ search.js
@@ -0,0 +1,2 @@
+const search = document.querySelector('.weather__searchform')
+const matchList = document.getElementById('match-list')
---
+++ utils.js
@@ -0,0 +1,4 @@
+const API_KEY = '4d04fcbccd9c491583085ab14c7639c2';
+const API_KEY2 = 'Ha+uL7PWniSxxh4GCAmWdQ==sPx1rvIoUPdum7L3';
+
+export {API_KEY}

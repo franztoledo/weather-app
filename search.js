158e4f6f04f327a38d76187ce0c4400a5fd1494b
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-19 06:50:52 -0500

    render matches list

--- search.js
+++ search.js
@@ -4,11 +4,28 @@ import { search, matchList } from "./index.js";
 const searchStates = async searchtText=>{
   const res = await fetch(`http://geodb-free-service.wirefreethought.com/v1/geo/places?limit=7&offset=0&types=CITY&namePrefix=${searchtText}&sort=-population`)
   const cities= await res.json()
-  const data = cities.data.map(city=>({
+  let data = cities.data.map(city=>({
     name: city.name,
     stateCode: city.regionCode,
     countryCode: city.countryCode
   }))
+  if(searchtText.length === 0){
+    data=[]
+    matchList.innerHTML=''
+  }
   console.log(data);
+  
+  outputHtml(data)
+}
+//show results in Html
+const outputHtml= matches=>{
+  if(matches.length > 0){
+    const html= matches.map(match=>`
+      <div class='search_match'>
+        <h4>${match.name},${match.stateCode},${match.countryCode}</h4>
+      </div>
+    `).join('')
+    matchList.innerHTML=html;
+  }
 }
 export {searchStates}
